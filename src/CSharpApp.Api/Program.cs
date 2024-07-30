61a2f1e65c6ea964c06ee0417e560c9191ad5e37
3IrB <irene.bouskou@gmail.com>
2024-07-30 21:45:17 +0300

    Add files via upload

--- src/CSharpApp.Api/Program.cs
+++ src/CSharpApp.Api/Program.cs
@@ -1,3 +1,8 @@
+using CSharpApp.Application.Services;
+using CSharpApp.Core.Dtos;
+using CSharpApp.Core.Interfaces;
+using Newtonsoft.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseSerilog(new LoggerConfiguration().ReadFrom.Configuration(builder.Configuration).CreateLogger());
@@ -36,6 +41,56 @@ app.MapGet("/todos/{id}", async ([FromRoute] int id, ITodoService todoService) =
         return todos;
     })
     .WithName("GetTodosById")
+.WithOpenApi();
+
+
+app.MapGet("/posts", async (ITodoPostService todopostService) =>
+{
+    var todos = await todopostService.GetAllTodos();
+    return todos;
+})
+    .WithName("GetPostTodos")
     .WithOpenApi();
+
+app.MapGet("/posts/{id}", async ([FromRoute] int id, ITodoPostService todopostService) =>
+{
+    var todos = await todopostService.GetTodoById(id);
+    return todos;
+})
+    .WithName("GetPostTodosById")
+.WithOpenApi();
+
+app.MapPost("/posts/{TodoRecord}", async (TodoRecord todoitem, ITodoPostService todopostService) =>
+{
+    TodoRecord todos = new(todoitem.UserId,todoitem.Id,todoitem.Title,true);
+    
+    
+    //TodoRecord vals = (TodoRecord)todoitem;
+    var todoos = await todopostService.postTodo(todoitem);
+    //return todos;
+    //var todoos = await todopostService.GetTodoById(todoitem.Id);
+    return todoos;
+})
+    .WithName("postTodo")
+    .WithOpenApi();
+
+app.MapDelete("/posts/{id}", async ([FromRoute] int id, ITodoPostService todopostService) =>
+{
+    var todos = await todopostService.deleteTodoById(id);
+    return todos;
+})
+    .WithName("deleteTodoById")
+    .WithOpenApi();
+
+
+//var todoItems = app.MapGroup("/todoitems");
+
+//todoItems.MapGet("/", GetTodos);
+////todoItems.MapGet("/complete", GetCompleteTodos);
+////todoItems.MapGet("/{id}", GetTodosById);
+//todoItems.MapPost("/", postTodoById);
+////todoItems.MapPut("/{id}", UpdateTodo);
+//todoItems.MapDelete("/{id}", deleteTodoById);
+
 
 app.Run();
