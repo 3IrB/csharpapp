8145e2cfaa59e07706e4fb4692e2456093400e86
3IrB <irene.bouskou@gmail.com>
2024-07-30 21:43:47 +0300

    Add files via upload

--- src/CSharpApp.Infrastructure/Configuration/DefaultConfiguration.cs
+++ src/CSharpApp.Infrastructure/Configuration/DefaultConfiguration.cs
@@ -5,7 +5,9 @@ public static class DefaultConfiguration
     public static IServiceCollection AddDefaultConfiguration(this IServiceCollection services)
     {
         services.AddSingleton<ITodoService, TodoService>();
-        
+
+        services.AddSingleton<ITodoPostService, TodoPostService>();
+
         return services;
     }
 }
